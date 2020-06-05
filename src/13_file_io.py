e70c1c9aa209c8cc2c61d8baa9b2e7e75cecb104
Ian <mus5002@aol.com>
2020-06-04 20:52:50 -0400

    args.py and file_io.py complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,6 +8,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
+file = open('foo.txt', 'r')
+file_content = file.read()
+print(file_content)
+file.close()
 
 # YOUR CODE HERE
 
@@ -15,5 +19,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
-
+new_file = open('bar.txt', 'w')
+new_file.write('Stuff \nin \na \nfile')
+new_file.close()
+f = open('bar.txt', 'r')
+f_content = f.read()
+print(f_content)
+f.close()
 # YOUR CODE HERE
